4bc4fc88a2f9bc3da0e5871538f1d227415a4173
“vjhawar12” <vedantj1203@gmail.com>
2023-08-30 13:45:17 -0400

    Fixed backend

--- index.js
+++ index.js
@@ -4,12 +4,44 @@ import axios from "axios";
 
 const port = 3000; 
 var app = express(); 
+const apiKey = "96955faeacdf481db33a71d13cb90368"; 
+
+const headers = {
+    "Content-Type": "application/json",
+    "apikey": apiKey,
+}; 
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended : true})); 
 
 app.get("/", (req, res) => {
     res.render("index.ejs"); 
+}); 
+
+app.post("/", async (req, res) => { 
+    console.log(req.body); 
+    const url = req.body.link;
+
+    async function shorten(url) {
+        let endpoint = "https://api.rebrandly.com/v1/links";
+        let linkRequest = {
+            destination: url,
+            domain: { fullName: "rebrand.ly" }
+        };
+        const apiCall = {
+            method: 'post',
+            url: endpoint,
+            data: linkRequest,
+            headers: headers
+        }; 
+        let apiResponse = await axios(apiCall);
+        let link = apiResponse.data;
+        return link.shortUrl;
+    }; 
+
+    let shortURL = await shorten(url); 
+    console.log(shortURL); 
+    res.render("index.ejs", { "shortURL" : shortURL}); 
 }); 
 
 app.listen(port, () => {
